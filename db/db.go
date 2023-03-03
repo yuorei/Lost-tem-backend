ff21a458752a30a0e8a6c315a12e87b558f138be
yuorei <yuorei71@gmail.com>
2023-03-03 15:41:48 +0900

    DB conect

---
+++ db/db.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Association struct {
+	DB    *gorm.DB
+	Error error
+}
+
+func (a Association) open() Association {
+	POSTGRES_HOST := os.Getenv("POSTGRES_HOST")
+	POSTGRES_USER := os.Getenv("POSTGRES_USER")
+	POSTGRES_PASSWORD := os.Getenv("POSTGRES_PASSWORD")
+	POSTGRES_DB := os.Getenv("POSTGRES_DB")
+	TZ := os.Getenv("TZ")
+	dsn := "host=" + POSTGRES_HOST + " user=" + POSTGRES_USER + " password=" + POSTGRES_PASSWORD + " dbname=" + POSTGRES_DB + " port=5432 sslmode=disable TimeZone=" + TZ
+	a.DB, a.Error = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return a
+}
