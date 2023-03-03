ddb1e9adf5e16771b2b3c5d8e357945624faadf5
yuorei <yuorei71@gmail.com>
2023-03-03 18:13:59 +0900

    DB周りの設定途中

--- back/db/db.go
+++ back/db/db.go
@@ -12,7 +12,8 @@ type Association struct {
 	Error error
 }
 
-func (a Association) open() Association {
+// DBと接続をします
+func (a Association) Open(){
 	POSTGRES_HOST := os.Getenv("POSTGRES_HOST")
 	POSTGRES_USER := os.Getenv("POSTGRES_USER")
 	POSTGRES_PASSWORD := os.Getenv("POSTGRES_PASSWORD")
@@ -20,5 +21,4 @@ func (a Association) open() Association {
 	TZ := os.Getenv("TZ")
 	dsn := "host=" + POSTGRES_HOST + " user=" + POSTGRES_USER + " password=" + POSTGRES_PASSWORD + " dbname=" + POSTGRES_DB + " port=5432 sslmode=disable TimeZone=" + TZ
 	a.DB, a.Error = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	return a
 }
--- back/main.go
+++ back/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"lost-item/db"
+
 	"github.com/gin-gonic/gin"
 )
-
+// サンプル
+type ss struct {
+	i int
+}
 func main() {
+	d := &db.Association{}
+	d.Open()
+	//サンプル
+	// テーブル
+	d.DB.AutoMigrate(&ss{})
+	// insert
+	d.DB.Create(&ss{
+		i:1,
+	})
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
