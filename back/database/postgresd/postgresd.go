822b0465775f358a0e6f4deb1708389323ef7479
motorailgun <28751910+motorailgun@users.noreply.github.com>
2023-03-04 17:40:17 +0900

    postgresd: ひとまず検索の実態を実装

--- back/database/postgresd/postgresd.go
+++ back/database/postgresd/postgresd.go
@@ -47,9 +47,34 @@ func (d *Postgresd) SearchItemsFor(query string) (model.SearchResult, error) {
 }
 
 func (d *Postgresd) SearchItemsArea(left_upper model.Location, right_bottom model.Location) (model.SearchResult, error) {
+	items := make([]model.LostItem, d.limit)
+	err := d.conn.Preload("Location").Where(
+		"Lat <= ? AND Lat >= ? AND Lng <= ? AND Lng >= ?",
+		left_upper.Lat, right_bottom.Lat, right_bottom.Lng, left_upper.Lng,
+	).Limit(int(d.limit)).Find(&items).Error
 
+	if err != nil {
+		return model.SearchResult{}, err
+	}
+
+	return model.SearchResult{
+		Count: uint(len(items)),
+		Items: items,
+	}, nil
 }
 
 func (d *Postgresd) ItemDetail(id uint64) (model.LostItem, error) {
+	item := model.LostItem{}
+	err := d.conn.Where("id = ?", id).First(&item).Error
 
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return model.LostItem{
+				Model: gorm.Model{ID: 0},
+			}, nil
+		}
+		return model.LostItem{}, err
+	}
+
+	return item, nil
 }
