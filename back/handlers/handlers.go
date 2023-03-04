0e71c29b4c020376846832ef37bed04b90ae35b7
yuorei <yuorei71@gmail.com>
2023-03-04 19:55:24 +0900

    改行追加

--- back/handlers/handlers.go
+++ back/handlers/handlers.go
@@ -86,17 +86,20 @@ func (h Handler) ItemDetail(c *gin.Context) {
 func (h Handler) RegisterItem(c *gin.Context) {
 	var err error
 	var res database.LostItem
-	register_item := model.LostItem{}
+	var register_item model.LostItem
+
 	err = c.Bind(&register_item)
 	if err != nil {
 		c.Status(http.StatusBadRequest)
 		return
 	}
-	res, err=h.db.InsertItem(register_item)
+
+	res, err = h.db.InsertItem(register_item)
 	if err != nil {
 		c.Status(http.StatusBadRequest)
 		return
 	}
+
 	c.JSON(http.StatusOK, res)
 
 }
