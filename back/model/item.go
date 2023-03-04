1e98a3b2bf80fdaa1ca0587710fbd76ea2ce404a
suzakutakumi <suzakutakumi3@gmail.com>
2023-03-04 22:33:02 +0900

    タイムの修正

--- back/model/item.go
+++ back/model/item.go
@@ -1,12 +1,14 @@
 package model
 
+import "time"
+
 type LostItem struct {
-	ID       uint     `json:"id"`
-	Kinds    []string `json:"tags"`
-	Comment  string   `json:"note"`
-	ImageURL string   `json:"pic"`
-	Location Location `json:"location"`
-	FindTime string   `json:"date"`
+	ID       uint      `json:"id"`
+	Kinds    []string  `json:"tags"`
+	Comment  string    `json:"note"`
+	ImageURL string    `json:"pic"`
+	Location Location  `json:"location"`
+	FindTime time.Time `json:"date" time_format:"2006-01-02T15:04:05Z"`
 }
 
 type Location struct {
