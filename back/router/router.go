019b0587f30b3e448a4c42d0979af656c6bc9e41
yuorei <yuorei71@gmail.com>
2023-03-04 09:17:13 +0900

    RouterのエンドポイントをAPI定義より追加

--- back/router/router.go
+++ back/router/router.go
@@ -10,6 +10,11 @@ func Router() *gin.Engine {
 	r := gin.Default()
 	h := handlers.Handler{}
 	h.Init()
-	r.GET("/serch",h.Search)
+	r.GET("/item_list",h.ItemList)
+	r.GET("/item",h.ItemDetail)
+	r.POST("/item",h.RegisterItem)
+	r.DELETE("/item",h.DeleteItem)
+	r.POST("/parse",h.Search)
+	r.POST(" /image",h.RegisterImage)
 	return r
 }
