e1215cc9b5eef19c99dbb8dec0a79a975d4b5a4a
yuorei <yuorei71@gmail.com>
2023-03-24 22:55:53 +0900

    LostItemにカラムを追加

--- back/database/table.go
+++ back/database/table.go
@@ -8,10 +8,14 @@ import (
 
 type LostItem struct {
 	gorm.Model
-	Kinds    string    `gorm:"not null"`
-	Comment  string    `gorm:"not null"`
-	ImageURL string    `gorm:"not null"`
-	Lat      float64   `gorm:"not null"`
-	Lng      float64   `gorm:"not null"`
-	FindTime time.Time `gorm:"not null"`
+	Kinds     string `gorm:"not null"`
+	Comment   string // 現在not null を記述していないが DBのtableではnut null となっている
+	ItemName  string
+	Colour    string
+	Situation string
+	Others    string
+	ImageURL  string    `gorm:"not null"`
+	Lat       float64   `gorm:"not null"`
+	Lng       float64   `gorm:"not null"`
+	FindTime  time.Time `gorm:"not null"`
 }
